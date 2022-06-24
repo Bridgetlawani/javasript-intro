a31d0fe0134c7de0f21d52c9ab238a31c613bf42
Bridget Lawani <105175902+Bridgetlawani@users.noreply.github.com>
2022-06-24 16:10:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ var Num2 = prompt("Enter second Number")
 if (input == "+"){
     var result = Number(Num1) + Number(Num2);
     alert("result is "+result)
-}else{
-    if (input == "-"){
-        var result = Number(Num1) - Number(Num2);
-        alert("result is "+result)
-    }
-    if (input == "*"){
-        var result = Number(Num1) - Number(Num2);
-        alert("result is "+result)
-    }
-    if (input == "/"){
-        var result = Number(Num1) - Number(Num2);
-        alert("result is "+result)
-    }
-}
+}else if(input == "-"){
+    var result = Number(Num1) - Number(Num2);
+    alert("result is "+result)
+}else if(input == "*"){
+    var result = Number(Num1) * Number(Num2);
+    alert("result is "+result)
+}else if(input == "/"){
+    var result = Number(Num1) / Number(Num2);
+    alert("result is "+result)
+}  
